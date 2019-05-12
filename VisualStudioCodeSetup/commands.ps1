14f4b11b6f7e3595ee43c90b7e29cd922e3a478e
haaranejarmo <jarmomct@outlook.com>
2019-05-12 21:48:20 +0300

    Update commands.ps1

--- VisualStudioCodeSetup/commands.ps1
+++ VisualStudioCodeSetup/commands.ps1
@@ -10,7 +10,6 @@ Invoke-WebRequest -uri $coreUrl -OutFile $file
 $arguments = '/i', "$file", '/qb-!'
 Start-Process 'msiexec.exe' -ArgumentList $arguments -NoNewWindow -Wait
 Start-Process 'C:\Program Files\PowerShell\6\pwsh.exe' -Wait -PassThru -NoNewWindow
-Exit
 #endregion PSCoreInstallation
 #region VSCodeInstallationAndConfiguration
 #VSCode Installation and configuration PowerShell Core commands
@@ -18,10 +17,15 @@ New-Item -Path C:\VisualStudioCode -Type Directory -Force
 Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?Linkid=852157 -OutFile C:\VisualStudioCode\VSCode.x64.exe
 C:\VisualStudioCode\VSCode.x64.exe /SILENT
 Install-Module -Name Az
-#Re-launch PowerShell Core console
-code --install-extensio nms-vscode.powershell
-code --install-extension ms-azuretools.vscode-azurefunctions
-code --list-extensions
+'C:\Program Files\Microsoft VS Code\bin\code.cmd' --install-extensio nms-vscode.powershell
+'C:\Program Files\Microsoft VS Code\bin\code.cmd' --install-extension ms-azuretools.vscode-azurefunctions
+'C:\Program Files\Microsoft VS Code\bin\code.cmd' --list-extensions
 Copy-Item "$Env:APPDATA\Code\User\settings.json" -Destination "$Env:APPDATA\Code\User\settings_orginal.json"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/haaranenjarmo/blog/15-5-2019/VisualStudioCodeSetup/settings.json -OutFile "$Env:APPDATA\Code\User\settings.json"
+#Launch VSCode
+code
+#Exit powerShell core
+Exit
+#Exit Windows PowerShell
+Exit
 #region VSCodeInstallationAndConfiguration
