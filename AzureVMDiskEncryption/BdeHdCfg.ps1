62883fac49f0f011bb534d4c7e96875044f37310
Jarmo Haaranen <44770817+haaranenjarmo@users.noreply.github.com>
2019-05-12 09:49:34 +0300

    File created

---
+++ AzureVMDiskEncryption/BdeHdCfg.ps1
@@ -0,0 +1,9 @@
+$azVm = "Enter Computer name or IP Address"
+$pathBdeHdCfg = "Enter local path where BdeHdCfg folder is"
+$cred = Get-Credential
+$session = New-PSSession -ComputerName 40.115.116.127 -Credential $cred -Authentication Default
+Copy-Item -Path "$pathBdeHdCfg\BdeHdCfg\BdeHdCfg.exe" -Destination C:\Windows\System32 -ToSession $session
+Copy-Item -Path "$pathBdeHdCfg\BdeHdCfg\BdeHdCfgLib.dll" -Destination C:\Windows\System32 -ToSession $session
+Copy-Item -Path "$pathBdeHdCfg\BdeHdCfg\en-Us\BdeHdCfg.exe.mui" -Destination C:\Windows\System32\en-US -ToSession $session
+Copy-Item -Path "$pathBdeHdCfg\BdeHdCfg\en-Us\BdeHdCfgLib.dll.mui" -Destination C:\Windows\System32\en-US -ToSession $session
+Invoke-Command -Session $session -ScriptBlock {bdehdcfg.exe -target default}
