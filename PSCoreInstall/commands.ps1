158bbb6e0c23be71f3901ec7f4b9213f98a1c81b
haaranejarmo <jarmomct@outlook.com>
2019-05-14 18:14:42 +0300

    Fix merge

--- PSCoreInstall/commands.ps1
+++ PSCoreInstall/commands.ps1
@@ -10,28 +10,3 @@ $arguments = '/i', "$file", '/qb-!'
 Start-Process 'msiexec.exe' -ArgumentList $arguments -NoNewWindow -Wait
 Start-Process 'C:\Program Files\PowerShell\6\pwsh.exe' -Wait -PassThru -NoNewWindow
 $PSVersionTable
-<<<<<<< HEAD
-#endregion PSCoreInstallation
-#region NetCoreInstallation
-"https://dotnet.microsoft.com/download/thank-you/dotnet-sdk-2.2.203-windows-x64-installer"
-#endregion NetCoreInstallation
-#region VSCodeInstallationAndConfiguration
-#VSCode Installation and configuration PowerShell Core commands
-New-Item -Path C:\VisualStudioCode -Type Directory -Force
-Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?Linkid=852157 -OutFile C:\VisualStudioCode\VSCode.x64.exe
-C:\VisualStudioCode\VSCode.x64.exe /SILENT
-Install-Module -Name Az
-'C:\Program Files\Microsoft VS Code\bin\code.cmd' --install-extensio nms-vscode.powershell
-'C:\Program Files\Microsoft VS Code\bin\code.cmd' --install-extension ms-azuretools.vscode-azurefunctions
-'C:\Program Files\Microsoft VS Code\bin\code.cmd' --list-extensions
-Copy-Item "$Env:APPDATA\Code\User\settings.json" -Destination "$Env:APPDATA\Code\User\settings_orginal.json"
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/haaranenjarmo/blog/15-5-2019/VisualStudioCodeSetup/settings.json -OutFile "$Env:APPDATA\Code\User\settings.json"
-#Launch VSCode
-code
-#Exit powerShell core
-Exit
-#Exit Windows PowerShell
-Exit
-#region VSCodeInstallationAndConfiguration
-=======
->>>>>>> 69757eea29304e52e9d8e0e540f7f73898ab22fa
